bb0f946ac479a31e8daab52e9ae519c668c9673d
gchinmayvarma <gchinmayvarma@gmail.com>
2023-05-04 01:51:02 +0530

    paint

--- script_links.js
+++ script_links.js
@@ -1,12 +1,19 @@
 let links = {
     "local": "http://localhost:5500/",
-    "github": "https://github.com/gchinmayvarma?tab=repositories&type=source",
+    "mail": "https://mail.google.com/mail/u/0/#inbox",
     "moodle": "https://wsdmoodle.waseda.jp/auth/saml2/login.php?wants=https%3A%2F%2Fwsdmoodle.waseda.jp%2F&idp=fcc52c5d2e034b1803ea1932ae2678b0&passive=off",
+    "github": "https://github.com/gchinmayvarma?tab=repositories&type=source",
+    "notes": "https://keep.google.com/u/0/",
     "paint": "https://gchinmayvarma.github.io/sketchoff/",
-    "chatgpt": "https://chat.openai.com/",
-    "youtube": "https://www.youtube.com/feed/subscriptions",
+    "cgpt": "https://chat.openai.com/",
+    "linkedin": "https://www.linkedin.com/feed/",
     "netflix": "https://www.netflix.com/",
+    "youtube": "https://www.youtube.com/feed/subscriptions",
     "rock": "https://www.youtube.com/playlist?list=PLHEO1tE3LKpJ6b6MXsMQ8dI5NDJWoSmCT",
+    "chess": "https://www.chess.com/play/online",
+    "type": "https://monkeytype.com/",
+    "airt": "https://lexica.art/",
+    "mods": "https://www.nexusmods.com/skyrimspecialedition/mods/2187",
 }
 
 let div_link = document.createElement('div');
