cdf6abf061fb1fe4e9cc4bc54a4ac693f97684b5
gchinmayvarma <gchinmayvarma@gmail.com>
2024-02-06 17:47:26 -0500

    offline cache

--- script_main.js
+++ script_main.js
@@ -7,6 +7,16 @@ function randint(min, max) {
 const PI = Math.PI;
 const TWO_PI = Math.PI * 2;
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  }
+  
 function showTime() {
     let time = new Date();
     let h = time.getHours();
---
+++ service-worker.js
@@ -0,0 +1,32 @@
+// service-worker.js
+
+const CACHE_NAME = 'my-website-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/script_gibs.js',
+  '/script_links.js',
+  '/script_main.js' ,
+  '/script_particles_image.js',
+  '/script_particles.js',
+  '/style.css'
+  // Add more resources to cache as needed
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
